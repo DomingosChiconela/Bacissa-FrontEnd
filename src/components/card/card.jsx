d0f121808d9f97857b79f0c269b6fae9194bccb6
Badrú <sbsbadroo@gmail.com>
2024-08-05 07:07:36 +0200

    fixed:cards-details

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import CardFt from '../Media/foto.jpeg';
 import { NumberFormatBase } from 'react-number-format';
 import { motion, AnimatePresence } from 'framer-motion';
